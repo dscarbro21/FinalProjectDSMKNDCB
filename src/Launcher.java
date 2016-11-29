71e67a41414da376af3740eba2492876c5317064
Michael Kelley <mikelley@adit.mines.edu>
2016-11-29 14:44:07 -0700

    Actually added launcher and star to github

---
+++ src/Launcher.java
@@ -0,0 +1,14 @@
+import java.awt.Point;
+
+public class Launcher {
+	
+	private Point position;
+	private int angle;
+	private int power;
+
+	public Launcher(int angle, int power) {
+		this.angle = angle;
+		this.power = power;
+	}
+
+}
---
+++ src/Star.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+
+public class Star {
+	
+	private Point position;
+	private Color color;
+	private int winRadius;
+
+	public Star(int r, Color c) {
+		color = c;
+		winRadius = r;
+	}
+
+}
