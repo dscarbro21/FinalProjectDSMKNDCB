0ffdad0e1de6906e146802441770cf281653d461
Daniel Scarbrough <dscarbro@mymayil.mines.edu>
2016-11-29 14:25:51 -0700

    Added ball class

---
+++ src/Ball.java
@@ -0,0 +1,20 @@
+import java.awt.Color;
+import java.awt.Point;
+
+public class Ball {
+
+	private Color color;
+	private int radius;
+	private int xVelocity;
+	private int yVelocity;
+	private Point position;
+	private int life;
+	
+	public Ball(int r, Color c) {
+		xVelocity = 0;
+		yVelocity = 0;
+		radius = r;
+		color = c;
+		life = 80;
+	}
+}
