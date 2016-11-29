884f39d77aaf57ff81e20d41dacd0e0929092372
Daniel Scarbrough <dscarbro@mymayil.mines.edu>
2016-11-29 14:48:28 -0700

    Added LevelGame class and bbasic gui

---
+++ src/LevelGame.java
@@ -0,0 +1,32 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class LevelGame extends JFrame {
+	private int currentLevel;
+	
+
+	public LevelGame() {
+		currentLevel = 1;
+
+		setSize(900, 900);
+		setTitle("Angular Deflection of Spherical Masses fun time"); //Probably change this?
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+//		JPanel panel = new DisplayPanel();
+//		add(panel);
+//		panel = new ColorChooser();
+//		add(panel);
+//		panel = new AngleChooser();
+//		add(panel); 
+	}
+
+	public void ChangeLevel() {
+		currentLevel++;
+	}
+
+	
+	public static void main(String [] args) {
+		LevelGame gui = new LevelGame();
+		gui.setVisible(true);
+	}
+}
