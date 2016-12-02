589d4e463593723612c3ebd606e97a31f2bcb9fc
Corbin Baker <corbinbaker@mymail.mines.edu>
2016-12-02 13:20:36 -0700

    JPanel extensions for GUI classes

--- src/AngleChooser.java
+++ src/AngleChooser.java
@@ -1,4 +1,5 @@
+import javax.swing.JPanel;
 
-public class AngleChooser {
+public class AngleChooser extends JPanel {
 
 }
--- src/DisplayPanel.java
+++ src/DisplayPanel.java
@@ -1,4 +1,5 @@
+import javax.swing.JPanel;
 
-public class DisplayPanel {
+public class DisplayPanel extends JPanel {
 
 }
